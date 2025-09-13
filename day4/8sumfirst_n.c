07936de29ecff27cb310626a9b00913308fb044b
rashiithakur001-cloud <rashiithakur001@gmail.com>
2025-09-13 23:04:48 +0530

    Add files via upload

---
+++ day4/8sumfirst_n.c
@@ -0,0 +1,12 @@
+//Write a program to find and display the sum of the first n natural numbers.
+
+#include <stdio.h>
+int main() {
+    int n, sum;
+    printf("Enter the numbers");
+    scanf("%d", &n);
+
+    sum = n * (n + 1) / 2;  
+    printf("Sum=%d\n", sum);
+  return 0;
+}
