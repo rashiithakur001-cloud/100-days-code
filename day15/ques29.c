0c8f08df89240de7927d6fc28370cfbb88f723dc
rashiithakur001-cloud <rashiithakur001@gmail.com>
2025-09-21 18:26:03 +0530

    Add files via upload

---
+++ day15/ques29.c
@@ -0,0 +1,17 @@
+//Write a program to calculate the factorial of a number.
+#include<stdio.h>
+int main(){
+    int n;
+    long long fact = 1; 
+   printf("Enter a number: ");
+    scanf("%d", &n);
+   if (n < 0) {
+    printf("not defined for negative numbers.\n");
+    } else {
+      for (int i = 1; i <= n; i++) {
+      fact *= i;
+    }
+     printf("%lld\n", fact);
+    }
+   return 0;
+}
