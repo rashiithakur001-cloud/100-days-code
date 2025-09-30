447da688fa8aae8799ced135506be543df444ddb
rashiithakur001-cloud <rashiithakur001@gmail.com>
2025-09-30 21:57:36 +0530

    Add files via upload

---
+++ day26/qs52.c
@@ -0,0 +1,41 @@
+/*Q52: Write a program to print the following pattern:
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*   */
+#include <stdio.h>
+
+int main() {
+    // Printing exactly as shown in the sample
+    printf("*\n");
+    printf("\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("\n");
+    printf("*\n");
+ return 0;
+}
